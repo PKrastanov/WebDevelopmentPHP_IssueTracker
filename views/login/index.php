6f0326b94014aeaad5a08931e32abfa26a9eb16b
PKrastanov <pesho1994@gmail.com>
2015-06-23 11:23:34 -0700

    Added register form.

--- views/login/index.php
+++ views/login/index.php
@@ -2,12 +2,12 @@
     <div class="container">
         <form class="form-horizontal">
             <fieldset>
-                <legend>Legend</legend>
+                <legend>Login</legend>
                 <form action="POST">
                     <div class="form-group">
-                        <label for="inputEmail" class="col-lg-2 control-label">Email</label>
+                        <label for="inputEmail" class="col-lg-2 control-label">Name</label>
                         <div class="col-lg-10">
-                            <input type="text" class="form-control" id="inputEmail" placeholder="Email">
+                            <input type="text" class="form-control" id="inputName" placeholder="Name">
                         </div>
                     </div>
                     <div class="form-group">
@@ -17,7 +17,7 @@
                         </div>
                     </div>
                     <div class="col-lg-10 col-lg-offset-2">
-                        <button type="submit" class="btn btn-primary">Submit</button>
+                        <button type="submit" class="btn btn-primary">Login</button>
                         <button type="button" class="btn pull-right" onclick="window.location='/register'">Register</button>
                     </div>
                 </form>
--- views/register/index.php
+++ views/register/index.php
@@ -1,3 +1,26 @@
-<?php
-
-echo "Welcome to Register Page.";
+<div class="jumbotron">
+    <div class="container">
+        <form class="form-horizontal">
+            <fieldset>
+                <legend>Register</legend>
+                <form action="POST">
+                    <div class="form-group">
+                        <label for="inputEmail" class="col-lg-2 control-label">Name</label>
+                        <div class="col-lg-10">
+                            <input type="text" class="form-control" id="inputName" placeholder="Name">
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label for="inputPassword" class="col-lg-2 control-label">Password</label>
+                        <div class="col-lg-10">
+                            <input type="password" class="form-control" id="inputPassword" placeholder="Password">
+                        </div>
+                    </div>
+                    <div class="col-lg-10 col-lg-offset-2">
+                        <button type="submit" class="btn btn-primary">Register</button>
+                    </div>
+                </form>
+            </fieldset>
+        </form>
+    </div>
+</div>
